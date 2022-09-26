e3a2af468f83dde775c2a5ed70f44d9adb366844
Ethan <esementelli@gmail.com>
2022-09-26 11:04:50 -0400

    location, user models

--- server/src/main/java/learn/game_finder/models/Location.java
+++ server/src/main/java/learn/game_finder/models/Location.java
@@ -1,4 +1,36 @@
 package learn.game_finder.models;
 
+import java.math.BigDecimal;
+
 public class Location {
+
+    private int locationId;
+    private double longitude;
+    private double latitude;
+
+
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 }
