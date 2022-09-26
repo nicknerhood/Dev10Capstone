0f6c2b1b688267e7824f577c73a8cb62b988ced0
Ethan <esementelli@gmail.com>
2022-09-26 12:49:07 -0400

    mappers and some repository for user done

---
+++ server/src/main/java/learn/game_finder/data/UserRepository.java
@@ -0,0 +1,18 @@
+package learn.game_finder.data;
+
+import learn.game_finder.models.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    List<User> findAll();
+
+    User findById(int userId);
+
+    User add(User user);
+
+    boolean update(User user);
+
+    boolean deleteById(int userId);
+}
