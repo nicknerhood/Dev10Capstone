cc6cd9903793baddf676a3f990275da6ab8d2d7f
Ethan <esementelli@gmail.com>
2022-09-26 20:52:09 -0400

    Location validations, still not sure how to handle deletion of location that is in use

--- server/src/main/java/learn/game_finder/domain/LocationService.java
+++ server/src/main/java/learn/game_finder/domain/LocationService.java
@@ -1,4 +1,51 @@
 package learn.game_finder.domain;
 
+import learn.game_finder.data.LocationRepository;
+import learn.game_finder.models.Location;
+
+import java.util.List;
+
 public class LocationService {
+
+private final LocationRepository repository;
+
+    public LocationService(LocationRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Location> findAll(){
+        return repository.findAll();
+    }
+
+    public Location findById(int locationId){
+        return repository.findById(locationId);
+    }
+
+    public boolean deleteById(int locationId){
+        if(repository.findIfInUse(locationId)){
+            return false;
+        }
+         return repository.deleteById(locationId);
+
+    }
+
+    private Result<Location> validate(Location location) {
+
+        Result<Location> result = new Result<>();
+        if (location == null) {
+            result.addMessage("Location cannot be null", ResultType.INVALID);
+            return result;
+        }
+        if (Validations.isNullOrBlank(String.valueOf(location.getLatitude()))) {
+            result.addMessage("latitude cannot be null or empty", ResultType.INVALID);
+        }
+        if (Validations.isNullOrBlank(String.valueOf(location.getLongitude()))) {
+            result.addMessage("longitude cannot be null or empty", ResultType.INVALID);
+        }
+
+        if(repository.findIfInUse(location.getLocationId())){
+            result.addMessage("Cannot delete Location that is used in a post or on a profile", ResultType.INVALID);
+        }
+        return result;
+    }
 }
--- server/src/test/java/learn/game_finder/domain/LocationServiceTest.java
+++ server/src/test/java/learn/game_finder/domain/LocationServiceTest.java
@@ -1,10 +1,14 @@
 package learn.game_finder.domain;
 
 import learn.game_finder.data.LocationRepository;
+import learn.game_finder.models.Location;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +20,7 @@ class LocationServiceTest {
 
     @Autowired
     LocationService service;
+
+
 
 }
