ed50d1d0f445d2ab86124407b1668120b936e546
Nick Nerhood <nrnerhood@gmail.com>
2022-09-30 14:04:52 -0500

    beginning map

---
+++ client/game-finder-react/src/components/PickUpMapTesting.js
@@ -0,0 +1,106 @@
+import {GoogleMap, LoadScript, Marker, InfoWindow} from '@react-google-maps/api';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import Game from './PickUp';
+
+const PickUpMapTesting = () => {
+
+    // const [pickups, setPickups] = useState([]);
+
+  //   const [selected, setSelected] = useState([]);
+
+    const onSelect = item => {
+        setSelected(item);
+    }
+
+    const mapStyles = {
+        height: "90vh",
+        width: "100%"
+    };
+    const defaultCenter = {
+        lat: 41.3851, lng: 2.1734
+    }
+
+  //   const locations = [
+  //       {
+  //           name: "Location 1",
+  //           location: { 
+  //             lat: 41.3954,
+  //             lng: 2.162 
+  //           },
+  //         },
+  //         {
+  //           name: "Location 2",
+  //           location: { 
+  //             lat: 41.3917,
+  //             lng: 2.1649
+  //           },
+  //         },
+  //         {
+  //           name: "Location 3",
+  //           location: { 
+  //             lat: 41.3773,
+  //             lng: 2.1585
+  //           },
+  //         },
+  //         {
+  //           name: "Location 4",
+  //           location: { 
+  //             lat: 41.3797,
+  //             lng: 2.1682
+  //           },
+  //         },
+  //         {
+  //           name: "Location 5",
+  //           location: { 
+  //             lat: 41.4055,
+  //             lng: 2.1915
+  //           },
+  //         }
+  //   ];
+
+  //   return (
+  //     <LoadScript googleMapsApiKey='AIzaSyB0CymM4J0zG7roy04odflwRmwvDz5MOfg'>
+  //             <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
+  //                 {
+  //                     locations.map(item => {
+  //                         return (
+  //                             <Marker draggable="true" key={item.name} position={item.location} onClick={() => onSelect(item)}/>
+  //                         )
+  //                     })
+  //                 }
+  //                 {
+  //                     selected.location &&
+  //                     (
+  //                         <InfoWindow position={selected.location} clickable={true} onCloseClick={() => setSelected({})} >
+  //                             <p>{selected.name}</p>
+  //                         </InfoWindow>
+  //                     )
+  //                 }
+  //             </GoogleMap>
+  //     </LoadScript>
+  // )
+
+  return (
+    <LoadScript googleMapsApiKey='AIzaSyB0CymM4J0zG7roy04odflwRmwvDz5MOfg'>
+            <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
+                {
+                    locations.map(item => {
+                        return (
+                            <Marker draggable="true" key={item.name} position={item.location} onClick={() => onSelect(item)}/>
+                        )
+                    })
+                }
+                {
+                    selected.location &&
+                    (
+                        <InfoWindow position={selected.location} clickable={true} onCloseClick={() => setSelected({})} >
+                            <p>{selected.name}</p>
+                        </InfoWindow>
+                    )
+                }
+            </GoogleMap>
+    </LoadScript>
+)
+}
+
+export default PickUpMapTesting;
